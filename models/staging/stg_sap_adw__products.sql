68a7a196aeecbcd74e45fb8415ae1aa2d2e1e55a
leandro.00@outlook.com.br <leandro.00@outlook.com.br>
2023-03-01 00:22:37 +0000

    Alterado stg products

--- models/staging/stg_sap_adw__products.sql
+++ models/staging/stg_sap_adw__products.sql
@@ -1,7 +1,7 @@
 -- Só foi usada as informações pertinentes com a regra de negócio.
 
 with
-    source_products as (
+    source_product as (
         select
            cast (productid as int) as product_id
            , cast (name as string) as name_product
@@ -9,4 +9,4 @@ with
     )
 
 select *
-from source_products
+from source_product
